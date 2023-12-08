40615c1cd5a60181255cfd9c2636a7439e01b733
Nathan Adams <dinnerbone@dinnerbone.com>
2023-12-08 18:55:58 +0100

    Removed temp code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -79,57 +79,6 @@ export default function Home() {
           </div>
         </div>
       </Container>
-
-      {/*<Space h="xl" />*/}
-      {/*<Space h="xl" />*/}
-      {/*<Space h="xl" />*/}
-      {/*<Space h="xl" />*/}
-      {/*<Space h="xl" />*/}
-      {/*<Space h="xl" />*/}
-
-      {/*<Grid gutter={80}>*/}
-      {/*  <GridCol span={4}>*/}
-      {/*    <Image*/}
-      {/*      src="/undraw/undraw_online_media_re_r9qv.svg"*/}
-      {/*      alt="Ruffle Logo"*/}
-      {/*      width={0}*/}
-      {/*      height={0}*/}
-      {/*      style={{ width: "100%", height: "auto" }} // optional*/}
-      {/*    />*/}
-      {/*  </GridCol>*/}
-      {/*  <GridCol span={8}>*/}
-      {/*    <p>*/}
-      {/*      Ruffle is a Flash Player emulator written in Rust. Ruffle runs*/}
-      {/*      natively on all modern operating systems as a standalone*/}
-      {/*      application, and on all modern browsers through the use of*/}
-      {/*      WebAssembly. Leveraging the safety of the modern browser sandbox and*/}
-      {/*      the memory safety guarantees of Rust, we can confidently avoid all*/}
-      {/*      the security pitfalls that Flash had a reputation for. Ruffle puts*/}
-      {/*      Flash back on the web, where it belongs - including browsers on iOS*/}
-      {/*      and Android!*/}
-      {/*    </p>*/}
-
-      {/*    <p>*/}
-      {/*      Designed to be easy to use and install, users or website owners may*/}
-      {/*      install the web version of Ruffle and existing flash content will*/}
-      {/*      "just work", with no extra configuration required. Ruffle will*/}
-      {/*      detect all existing Flash content on a website and automatically*/}
-      {/*      "polyfill" it into a Ruffle player, allowing seamless and*/}
-      {/*      transparent upgrading of websites that still rely on Flash content.*/}
-      {/*    </p>*/}
-
-      {/*    <p>*/}
-      {/*      Ruffle is an entirely open source project maintained by volunteers.*/}
-      {/*      We're all passionate about the preservation of internet history, and*/}
-      {/*      we were drawn to working on this project to help preserve the many*/}
-      {/*      websites and plethora of content that will no longer be accessible*/}
-      {/*      when users can no longer run the official Flash Player. If you would*/}
-      {/*      like to help support this project, we welcome all contributions of*/}
-      {/*      any kind - even if it's just playing some old games and seeing how*/}
-      {/*      well they run.*/}
-      {/*    </p>*/}
-      {/*  </GridCol>*/}
-      {/*</Grid>*/}
     </Container>
   );
 }
